4f6a92319b19537b76d0882c582bc719c326537f
doaa dewedar <doaadewedar63@gmail.com>
2023-06-15 12:20:38 -0700

    ghghjgjhg

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Size of char: %zu byte%s\n", sizeof(char), sizeof(char) > 1 ? "s" : "");
-    printf("Size of int: %zu byte%s\n", sizeof(int), sizeof(int) > 1 ? "s" : "");
-    printf("Size of long: %zu byte%s\n", sizeof(long), sizeof(long) > 1 ? "s" : "");
-    printf("Size of long long: %zu byte%s\n", sizeof(long long), sizeof(long long) > 1 ? "s" : "");
+    printf("Size of a char: %zu byte%s\n", sizeof(char), sizeof(char) > 1 ? "s" : "");
+    printf("Size of an int: %zu byte%s\n", sizeof(int), sizeof(int) > 1 ? "s" : "");
+    printf("Size of long int: %zu byte%s\n", sizeof(long), sizeof(long) > 1 ? "s" : "");
+    printf("Size of long long int: %zu byte%s\n", sizeof(long long), sizeof(long long) > 1 ? "s" : "");
     printf("Size of float: %zu byte%s\n", sizeof(float), sizeof(float) > 1 ? "s" : "");
 
     return 0;
