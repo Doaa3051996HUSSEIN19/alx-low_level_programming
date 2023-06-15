1436e91b1ae448da598358a6387661e8f9a4d3fb
doaa dewedar <doaadewedar63@gmail.com>
2023-06-15 10:52:53 -0700

    gjhghgkjgkj

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
