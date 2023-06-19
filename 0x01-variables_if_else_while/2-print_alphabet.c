90e4a3c43759b32086272ee85255d794e1d97947
doaa dewedar <doaadewedar63@gmail.com>
2023-06-19 12:12:32 -0700

    :jhghghjghj

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: if loop
+*
+*Return: Always 0 (Success)
+**/
+int main(void)
+{
+	int  alpha = "a";
+
+	while (alpha <= "z")
+	{
+		putchar (alpha);
+		alpha++;
+	}
+	putchar ("\n");
+	return (0);
+}
