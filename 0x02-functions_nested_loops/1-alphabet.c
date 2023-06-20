b77a7e5601a310da9a9d3bec72782072bc246501
doaa dewedar <doaadewedar63@gmail.com>
2023-06-20 11:09:19 -0700

    jhjkhkjhkj

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
-
-
 /**
-* print alphabet - Make the alphabet 
+* print_alphabet - Make the alphabet
 * Return: void
 **/
 
 void print_alphabet(void)
-
-{	
+{
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
