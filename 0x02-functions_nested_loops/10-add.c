3c6bd79a773cd1f4b47dde3d8bf7099625a8ad0a
doaa dewedar <doaadewedar63@gmail.com>
2023-06-21 11:49:51 -0700

    kjkljlk

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - a function that adds two integers
+* and returns the result.
+*
+* @a: number one.
+* @b: number two.
+*
+* Return: Add of number one and number two.
+*/
+int add(int n2, int n1)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_to_98 - prints all natural numbers
+* from n to 98, followed by a new line
+*
+* @n: input number.
+*
+* Return: no return.
+*/
+void jack_bauer(void)
+{
+
