cbf4093ebfc0acfbdf93cddc0e7763c35b530603
doaa dewedar <doaadewedar63@gmail.com>
2023-06-21 12:47:23 -0700

    jhjhkj

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,27 @@
 #include "main.h"
+
 /**
-* print_to_98 - prints all natural numbers
-* from n to 98, followed by a new line
-*
-* @n: input number.
-*
-* Return: no return.
-*/
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
 void jack_bauer(void)
 {
+	int i, j;
 
+	i = 0;
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
