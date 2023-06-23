91d509fd4796d99f91f5a0105ffe4bf7b6d92c1c
doaa dewedar <doaadewedar63@gmail.com>
2023-06-23 14:21:39 -0700

    hghjg

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,11 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = '0' ; num < 10 ; num++)
+	for (num = '0' ; num < '9' ; num++)
 	{
 		if (num == '2' || num == '4')
-			;
-		else
-			_putchar (num);
+			continue;
+		_putchar (num);
 	}
 	_putchar ('\n');
 }
